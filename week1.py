e759ee81930e6b90320f4583890501713678fffa
korysanchez <99495630+korysanchez@users.noreply.github.com>
2022-02-21 23:23:17 -0500

    Accounted for other good colors

--- week1.py
+++ week1.py
@@ -1,6 +1,6 @@
 name = input("What's your name?")
 color = input("What's your favorite color?")
-if (color != "red"):
+if (color != "red") or (color != "blue"):
         print("Well, " + name + ", your favorite color is lame.")
 else:
         print("Well, " + name + ", your favorite color is valid!")
